a9cf5e5c0e8e9b168bed2e08fe8fd2ad5c9ed6eb
Skjamal <satinder.jamal@gmail.com>
2016-04-14 19:51:20 +0000

    Create Edit Page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -24,6 +24,16 @@ def place_params
   params.require(:place).permit(:name, :description, :address)
 end
 
+def edit
+  @place = Place.find(params[:id])
+end
+
+def update
+  @place = Place.find(params[:id])
+  @place.update_attributes(place_params)
+  redirect_to root_path
+end 
+
 end
 
 
