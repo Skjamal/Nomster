6d3788dfb122f1a452cd48618d5c518def38d67f
Skjamal <satinder.jamal@gmail.com>
2016-04-14 21:21:21 +0000

    Made changes

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -17,22 +17,24 @@ class PlacesController < ApplicationController
   def show
     @place = Place.find(params[:id])
   end
-
-private
-
-def place_params
-  params.require(:place).permit(:name, :description, :address)
-end
-
-def edit
+ 
+ def edit
   @place = Place.find(params[:id])
-end
+ end
 
-def update
-  @place = Place.find(params[:id])
-  @place.update_attributes(place_params)
-  redirect_to root_path
-end 
+ def update
+    @place = Place.find(params[:id])
+    @place.update_attributes(place_params)
+    redirect_to root_path
+  end
+  
+ private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
+
+
 
 end
 
