d81e20fe654d97d0de05462bf62d6c46f92659eb
Skjamal <satinder.jamal@gmail.com>
2016-04-16 22:24:01 +0000

    Add comments feature

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,8 +1,10 @@
 class Place < ActiveRecord::Base
   belongs_to :user
+  has_many :comments
+  
   geocoded_by :address
   after_validation :geocode
-  
+
   validates :name,:description,:address,:presence => true
   validates :name,:length => { minimum: 3 }
 end
